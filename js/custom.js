2ee90a0280b66c101e927722a9e3cc0eb6a8154f
ohbe1 <47062393+ohbe1@users.noreply.github.com>
2019-01-26 21:39:09 +0000

    Initial Commit
    First Commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+   
+    //alert('test');
+   
+   
+//$("[id^=btn]").on('click',function(){
+    
+   
+  //  $(this).toggleClass(function(){
+  //  return $(this).is('.btn-outline-success') ? 'btn-success' : 'btn-outline-success';
+ // });
+   
+    /*$('#'+this.id).hasClass( "foo" )
+    $('#'+this.id+'.btn-outline-primary.btn-primary').toggleClass("btn-outline-primary btn-primary");
+    $('#'+this.id+'.btn-outline-success.btn-success').toggleClass("btn-outline-success btn-success");
+    $('#'+this.id+'.btn-outline-warning.btn-warning').toggleClass("btn-outline-warning btn-warning");
+    $('#'+this.id+'.btn-outline-danger.btn-danger').toggleClass("btn-outline-danger btn-danger");
+    */
+ 
+   
+
+ 
+
+  
+//});
+
+}
+       
+       
+        );
+
